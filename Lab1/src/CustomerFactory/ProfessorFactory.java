4974dffbed86bc1e2681d2c03fee3207040d4289
leopold1511 <bavrinleonid@gmail.com>
2024-03-24 15:33:00 +0300

    Add surnames for professors

--- Lab1/src/CustomerFactory/ProfessorFactory.java
+++ Lab1/src/CustomerFactory/ProfessorFactory.java
@@ -1,5 +1,7 @@
 package CustomerFactory;
+
 import Customer.*;
+
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
@@ -8,18 +10,25 @@ public class ProfessorFactory implements CustomerFactory {
     public Customer CreateCustomer(List<String[]> listOfNames, List<String[]> listOfSurnames) {
         Random r = new Random();
         String[] name = listOfNames.get(r.nextInt(listOfNames.size() - 1));
-        String surname = listOfSurnames.get(r.nextInt(listOfNames.size() - 1))[0];
+        String surname = listOfSurnames.get(r.nextInt(listOfSurnames.size() - 1))[0];
         String[] nameForPatronymics;
         String patronymics;
         do {
             nameForPatronymics = listOfNames.get(r.nextInt(listOfNames.size() - 1));
         } while (Objects.equals(nameForPatronymics[1], "F"));
         if (Objects.equals(name[1], "F")) {
-            surname = surname + "а";
+            if (checkSurname(surname)) surname = surname + "а";
             patronymics = nameForPatronymics[3];
         } else patronymics = nameForPatronymics[2];
-        Professor customer = new Professor(name[0], patronymics,surname);
+        Professor customer = new Professor(name[0], patronymics, surname);
         return customer;
+    }
+
+    private boolean checkSurname(String surname) {
+        if (surname.substring(surname.length() - 1).equals("о") ||
+                surname.substring(surname.length() - 1).equals("ь"))
+            return false;
+        return true;
     }
 }
 
--- Lab1/src/CustomerFactory/StudentFactory.java
+++ Lab1/src/CustomerFactory/StudentFactory.java
@@ -11,7 +11,7 @@ public class StudentFactory implements CustomerFactory {
     public Customer CreateCustomer(List<String[]> listOfNames, List<String[]> listOfSurnames) {
         Random r = new Random();
         String[] name = listOfNames.get(r.nextInt(listOfNames.size() - 1));
-        String surname = listOfSurnames.get(r.nextInt(listOfNames.size() - 1))[0];
+        String surname = listOfSurnames.get(r.nextInt(listOfSurnames.size() - 1))[0];
         if (Objects.equals(name[1], "F")) surname = surname + "а";
         Student customer = new Student(name[0], surname);
         return customer;
